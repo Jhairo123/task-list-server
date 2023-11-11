a8caf2a4ebf1c768dcfdaf0a4027640452fd1b94
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2023-11-11 16:54:32 -0500

    Implement REST API

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const { JWTValidation, authenticateUser } = require("./middleware/middle.js");
-const listViewRouter = require("./routes/list-view-router.js");
 const listEditRouter = require("./routes/list-edit-router.js");
 const tasks = require("../utils/tasks.json");
 
@@ -10,26 +9,18 @@ require("dotenv").config();
 app.use(express.json());
 const port = process.env.PORT;
 
+app.use("/tasks", listEditRouter);
+
+const router = express.Router();
+
 const users = [
   { id: 1, email: "correo@gmail.com", password: "contrasena1", rol: "user" },
   { id: 2, email: "correo1@gmail.com", password: "contrasena2", rol: "user" },
 ];
 
-/**
- *
- * Middleware to validate permitted HTTP methods(GET, POST, PUT o DELETE).
- *
- * @param {number} req - The object of the Express request.
- * @param {object} res - The object of the response Express.
- * @param {function} next - The next middleware function.
- *
- * @returns {void} - It does not return any value directly. If the method is valid, it is passed to the next middleware.
- *                   If the method is invalid, an error is returned and no progress is made to the next middleware.
- */
-
 app.post("/login", authenticateUser, (req, res) => {
   const USER_EMAIL_BD = "correo@gmail.com";
-  const USER_PASS_DB = "unodostres";
+  const USER_PASS_DB = "contrasena1";
   const dataUser = req.body;
 
   if (dataUser.email == USER_EMAIL_BD && dataUser.password == USER_PASS_DB) {
@@ -40,9 +31,7 @@ app.post("/login", authenticateUser, (req, res) => {
       password: users[index].password,
       rol: users[index].rol,
     };
-    token = jwt.sign(payload, process.env.SECRET_KEY, {
-      expiresIn: "1m",
-    });
+    token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "24h" });
   }
   return res.json({ token });
 });
@@ -66,60 +55,33 @@ app.use("/", JWTValidation, (req, res, next) => {
   next();
 });
 
-/**
- * Middleware for handling endpoint validation.
- *
- * This middleware checks the provided endpoint parameter and enforces specific rules for certain endpoints.
- *
- * @param {object} req - The Express request object.
- * @param {object} res - The Express response object.
- * @param {function} next - The next middleware function.
- *
- * @returns {void} - No return value directly. It either continues to the next middleware or sends a response.
- */
 app.use("/:endpoint", (req, res, next) => {
   const endpoint = req.params.endpoint;
-  if (endpoint === "tasks" || endpoint === "task") {
+  if (endpoint === "tasks" || endpoint === "login") {
     if (!tasks.length == 0) return next();
     return res.status(400).send({
       error: "There are no tasks found",
       message: "You must create at least one task to be able to display it",
     });
-  } else res.status(400).send({ error: "invalid endpoint " + "/" + endpoint });
+  } else
+    res.status(400).send({
+      error: "404 - Not found ",
+      login: "http://localhost:3000/login",
+    });
 });
 
-/**
- * HTTP GET method to get a tasks list.
- *
- * @route GET /tasks
- * @returns {JSON} - An object containing the list of tasks.
- */
-app.get("/tasks", JWTValidation, (req, res) => {
-  return res.status(200).send({ tasks: tasks });
-});
-
-app.use("/tasks", listViewRouter);
-app.use("/task", listEditRouter);
-
-/**
- * Starts the server to listen for incoming requests.
- *
- * @param {number} port - The port on which the server will run.
- */
 app.listen(port, () => {
   console.log(`Server on`);
   console.log(
     `Get task list: http://localhost:${port}/tasks` +
       "\n" +
-      `Get task list completed: http://localhost:${port}/tasks/completed` +
+      `Get task list completed: http://localhost:${port}/tasks` +
       "\n" +
-      `Get task list completed: http://localhost:${port}/tasks/incomplete`
+      `Get task list completed: http://localhost:${port}/tasks?completed=false`
   );
 });
 
 //PUT http://localhost:3000/task/2 {  "title"="Task 1", "description"="Write a new task" }
 //DELETE http://localhost:3000/task/2
 
-//No puedo acceder a los middlewares
-//Error: Cannot find module '../middleware/middle.js'
 module.exports = app;
